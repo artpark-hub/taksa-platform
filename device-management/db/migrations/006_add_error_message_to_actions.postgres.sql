a1df35096346bbc90b9a13721000f7c52a4b3c62
Chetan Kumar S <chetan@artpark.in>
2026-04-10 11:15:42 +0530

    Fixed based on commemts from https://github.com/artpark-hub/taksa-platform/pull/12

--- device-management/db/migrations/006_add_error_message_to_actions.postgres.sql
+++
@@ -1,4 +0,0 @@
--- Migration: Add error_message column to actions table (PostgreSQL)
--- Purpose: Store error details when actions fail
-
-ALTER TABLE actions ADD COLUMN IF NOT EXISTS error_message TEXT;
